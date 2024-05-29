17db0c3570f75b82259acc332a12ded213abda58
jackgusler <guslerj1@newpaltz.edu>
2024-05-29 12:14:36 -0400

    positioned nonogram in the middle

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import './index.css';
 import React from 'react';
 import Nono from './p5/sketch';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Nono />
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="container mx-auto flex justify-center items-center">
+        <Nono />
+      </div>
     </div>
   );
 }
