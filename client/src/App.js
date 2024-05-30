e0969798d0d3943fe5e64690fa000086c6a1fb7f
jackgusler <guslerj1@newpaltz.edu>
2024-05-30 14:27:53 -0400

    removed p5 project, converting it to just react to make the syling better

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
-import Nono from "./p5/sketch";
+import React from "react";
 import "./index.css";
 
 function App() {
-  const [image, setImage] = useState(null);
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="container mx-auto flex">
         <div className="w-1/2 flex justify-center items-center">
-          {image && <img src={image.canvas.toDataURL()} alt="Random" />}
+          <img src="" alt="Random" />
         </div>
         <div className="w-1/2 flex justify-center items-center">
-          <Nono setImage={setImage} />
+          
         </div>
       </div>
     </div>
