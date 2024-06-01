15ef623c26b4884339a2bd2fa9cac298c10ab09c
jackgusler <guslerj1@newpaltz.edu>
2024-06-01 18:10:52 -0400

    added buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,141 +1,35 @@
 import "./index.css";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./components/Button";
 
 function App() {
-  let diffuculty;
-  let category;
+  const [difficulty, setDifficulty] = useState(null);
+  const [category, setCategory] = useState(null);
 
   return (
-    // <div className="flex justify-center items-center min-h-screen">
-    //   <div className="container mx-auto flex">
-    //     <div className="w-1/2 flex justify-center items-center">
-    //       <img src="" alt="Random" />
-    //     </div>
-    //     <div className="w-1/2 flex justify-center items-center">
-
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen flex-col space-y-12">
       <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="blue"
-          onClick={() => diffuculty = "easy"}
-        >
-          Easy
+        <Button color="blue" onClick={() => setCategory("MLB")}>
+          MLB
         </Button>
-        <Button
-          color="blue"
-          onClick={() => diffuculty = "medium"}
-        >
-          Medium
+        <Button color="red" onClick={() => setCategory("NBA")}>
+          NBA
         </Button>
-        <Button
-          color="blue"
-          onClick={() => diffuculty = "hard"}
-        >
-          Hard
+        <Button color="green" onClick={() => setCategory("NFL")}>
+          NFL
+        </Button>
+        <Button color="black" onClick={() => setCategory("NHL")}>
+          NHL
         </Button>
       </div>
       <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="green"
-          onClick={() => diffuculty = "easy"}
-        >
+        <Button color="grey" onClick={() => setDifficulty("easy")}>
           Easy
         </Button>
-        <Button
-          color="green"
-          onClick={() => diffuculty = "medium"}
-        >
+        <Button color="grey" onClick={() => setDifficulty("medium")}>
           Medium
         </Button>
-        <Button
-          color="green"
-          onClick={() => diffuculty = "hard"}
-        >
-          Hard
-        </Button>
-      </div>
-      <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="red"
-          onClick={() => diffuculty = "easy"}
-        >
-          Easy
-        </Button>
-        <Button
-          color="red"
-          onClick={() => diffuculty = "medium"}
-        >
-          Medium
-        </Button>
-        <Button
-          color="red"
-          onClick={() => diffuculty = "hard"}
-        >
-          Hard
-        </Button>
-      </div>
-      <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="black"
-          onClick={() => diffuculty = "easy"}
-        >
-          Easy
-        </Button>
-        <Button
-          color="black"
-          onClick={() => diffuculty = "medium"}
-        >
-          Medium
-        </Button>
-        <Button
-          color="black"
-          onClick={() => diffuculty = "hard"}
-        >
-          Hard
-        </Button>
-      </div>
-      <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="white"
-          onClick={() => diffuculty = "easy"}
-        >
-          Easy
-        </Button>
-        <Button
-          color="white"
-          onClick={() => diffuculty = "medium"}
-        >
-          Medium
-        </Button>
-        <Button
-          color="white"
-          onClick={() => diffuculty = "hard"}
-        >
-          Hard
-        </Button>
-      </div>
-      <div className="container mx-auto flex justify-center space-x-4">
-        <Button
-          color="grey"
-          onClick={() => diffuculty = "easy"}
-        >
-          Easy
-        </Button>
-        <Button
-          color="grey"
-          onClick={() => diffuculty = "medium"}
-        >
-          Medium
-        </Button>
-        <Button
-          color="grey"
-          onClick={() => diffuculty = "hard"}
-        >
+        <Button color="grey" onClick={() => setDifficulty("hard")}>
           Hard
         </Button>
       </div>
